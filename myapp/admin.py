5c0c8569941128dcae404b472628fe08e9ce6008
shahid <shahid.ice.nstu@gmail.com>
2021-12-06 14:37:37 +0600

    random

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post
+
+# admin.site.register(Post)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Post._meta.get_fields()]
--- src/settings.py
+++ src/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'myapp.middleware.middleware_factory',
 ]
 
 ROOT_URLCONF = 'src.urls'
