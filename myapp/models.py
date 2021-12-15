5be43d7099f0f1955318de02090ffd5b735c9aa6
shahid <shahid.ice.nstu@gmail.com>
2021-12-15 12:14:45 +0600

    play with abstract model class

--- myapp/models.py
+++ myapp/models.py
@@ -4,3 +4,16 @@ from django.db import models
 class Post(models.Model):
     title = models.TextField(max_length=200)
     content = models.TextField()
+
+
+class CommonInfo(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+
+    class Meta:
+        abstract = True
+
+
+class Student(CommonInfo):
+    name = None
+    home_group = models.CharField(max_length=5)
