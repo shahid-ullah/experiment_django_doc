90b684b0be25436121c943e3937fe6b35f957d6b
shahid <shahid.ice.nstu@gmail.com>
2022-02-15 16:02:12 +0600

    Created model instance dynamically

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,77 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+import io
+
+from django.http import FileResponse, HttpResponse
 from django.shortcuts import render
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.units import inch
+from reportlab.pdfgen import canvas
+
+# from .forms import DateForm
 
 
 def home(request):
-    return HttpResponse('Hello world')
+    # Create a file-like buffer to receive PDF data.
+    # buffer = io.BytesIO()
+
+    # # Create the PDF object, using the buffer as its "file."
+    # p = canvas.Canvas(buffer, pagesize=letter, bottomup=0)
+    # text_obj = p.beginText()
+    # text_obj.setTextOrigin(inch, inch)
+    # text_obj.setFont('Helvetica', 14)
+
+    # lst = [
+    #     'this is first line',
+    #     'this is second line',
+    #     'this is third line',
+    # ]
+    # for line in lst:
+    #     text_obj.textLine(line)
+
+    # p.drawText(text_obj)
+    # p.showPage()
+    # p.save()
+    # buffer.seek(0)
+
+    # # Draw things on the PDF. Here's where the PDF generation happens.
+    # # See the ReportLab documentation for the full list of functionality.
+    # # p.drawString(100, 100, "Hello world.")
+    # # p.drawString(100, 200, "Hello world.")
+    # # p.drawString(100, 300, "Hello world.")
+
+    # # # Close the PDF object cleanly, and we're done.
+    # # p.showPage()
+    # # p.save()
+
+    # # FileResponse sets the Content-Disposition header so that browsers
+    # # present the option to save the file.
+    # buffer.seek(0)
+    # return FileResponse(buffer, as_attachment=False, filename='hello.pdf')
+    return render(request, 'home.html')
+
+
+def format_data(request):
+
+    # Create a file-like buffer to receive PDF data.
+    buffer = io.BytesIO()
+
+    # # Create the PDF object, using the buffer as its "file."
+    p = canvas.Canvas(buffer, pagesize=letter, bottomup=0)
+    text_obj = p.beginText()
+    text_obj.setTextOrigin(inch, inch)
+    text_obj.setFont('Helvetica', 14)
+
+    lst = [
+        'this is first line',
+        'this is second line',
+        'this is third line',
+    ]
+    for line in lst:
+        text_obj.textLine(line)
+
+    p.drawText(text_obj)
+    p.showPage()
+    p.save()
+    buffer.seek(0)
+
+    return FileResponse(buffer, as_attachment=False, filename='hello.pdf')
